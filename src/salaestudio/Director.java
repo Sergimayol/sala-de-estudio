805e9d365acc1cb502d8487b774699d87e4229e2
Sergi Mayol <sergi2808@gmail.com>
2022-11-16 10:04:01 +0100

    Update Director y estudiante

--- src/salaestudio/Director.java
+++ src/salaestudio/Director.java
@@ -1,5 +1,17 @@
 package salaestudio;
 
-public class Director {
+public class Director implements Runnable {
+
+    private SalaEstudio sala;
+
+    public Director(SalaEstudio sala) {
+        this.sala = sala;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hola, soy el director");
+        System.out.println(sala.getMensaje());
+    }
     
 }
--- src/salaestudio/Estudiante.java
+++ src/salaestudio/Estudiante.java
@@ -3,9 +3,11 @@ package salaestudio;
 public class Estudiante implements Runnable {
 
     private final String id;
+    private SalaEstudio sala;
 
-    public Estudiante(String id) {
+    public Estudiante(String id, SalaEstudio sala) {
         this.id = id;
+        this.sala = sala;
     }
 
     public String getId() {
@@ -15,6 +17,7 @@ public class Estudiante implements Runnable {
     @Override
     public void run() {
         System.out.println("Hola, soy " + this.id);
+        System.out.println(sala.getMensaje());
     }
 
 }
